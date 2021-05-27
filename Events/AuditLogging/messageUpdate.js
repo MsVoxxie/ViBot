367a98f940cbd754bf59072173fe7542fc72b4a1
MsVoxxie <odysseylove222@gmail.com>
2021-05-27 02:00:31 -0400

    PARTIAL fixes

--- Events/AuditLogging/messageUpdate.js
+++ Events/AuditLogging/messageUpdate.js
@@ -7,21 +7,21 @@ module.exports = {
 	async execute(oldMessage, newMessage, bot) {
 
 		// If Partial, Fetch
-		if(oldMessage.partial) { await oldMessage.fetch(); }
+		if(oldMessage.partial) { await newMessage.fetch(); }
 
 		// Declarations / Checks
 		const settings = await bot.getGuild(oldMessage.guild);
 		if(!settings) return;
 		if(settings.audit === false) return;
-		if(oldMessage.author.bot) return;
-		if(oldMessage.channel.type !== 'text') return;
-		if (oldMessage.content === newMessage.content) return;
-		const logChannel = await oldMessage.guild.channels.cache.get(settings.auditchannel);
+		if(newMessage.author.bot) return;
+		if(newMessage.channel.type !== 'text') return;
+		if (newMessage.content === newMessage.content) return;
+		const logChannel = await newMessage.guild.channels.cache.get(settings.auditchannel);
 
 		// Setup Embed
 		const embed = new MessageEmbed()
 			.setTitle('Message Updated')
-			.setDescription(`**Author›** <@${oldMessage.author.id}> | **${oldMessage.author.tag}**\n**Channel›** <#${oldMessage.channel.id}> | **${oldMessage.channel.name}**\n**Message Link›** [Jump To Message](${newMessage.url})\n\n**Original Message›**\`\`\`${oldMessage.content.replace(/`/g, '\'')}\`\`\`\n\n**Updated Message›**\`\`\`${newMessage.content.replace(/`/g, '\'')}\`\`\``)
+			.setDescription(`**Author›** <@${newMessage.author.id}> | **${newMessage.author.tag}**\n**Channel›** <#${newMessage.channel.id}> | **${newMessage.channel.name}**\n**Message Link›** [Jump To Message](${newMessage.url})\n\n**Original Message›**\`\`\`${oldMessage.content.replace(/`/g, '\'')}\`\`\`\n\n**Updated Message›**\`\`\`${newMessage.content.replace(/`/g, '\'')}\`\`\``)
 			.setColor(settings.guildcolor)
 			.setFooter(`Updated› ${bot.Timestamp(newMessage.createdAt)}`);
 
--- Vi.js
+++ Vi.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 require('discord-reply');
 const { Token } = require('./Storage/Config/Config.json');
-const bot = new Discord.Client({ partials: ['REACTION'] });
+const bot = new Discord.Client({ partials: ['REACTION', 'MESSAGE'] });
 
 // Command Info
 bot.commands = new Discord.Collection();
