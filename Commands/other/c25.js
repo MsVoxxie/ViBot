b093aad9a9a1788ce4bf3652c78d29e8121d588c
MsVoxxie <odysseylove222@gmail.com>
2021-09-23 22:26:13 -0400

    Unlisted c25 command for server stats.

---
+++ Commands/other/c25.js
@@ -0,0 +1,52 @@
+const { MessageEmbed } = require('discord.js');
+const mcUtil = require('minecraft-server-util');
+
+module.exports = {
+	name: 'c25',
+	aliases: [],
+	description: 'Checks if the Creation 2.5 server status.',
+	example: '',
+	category: 'other',
+	args: false,
+	cooldown: 0,
+	hidden: true,
+	ownerOnly: false,
+	userPerms: [],
+	botPerms: [],
+	async execute(bot, message, args, settings, Vimotes) {
+		if (settings.audit) {
+			setTimeout(() => message.delete(), 30 * 1000);
+		}
+
+		mcUtil
+			.status('167.114.48.196')
+			.then((response) => {
+				const host = response.host;
+				const port = '25565';
+				const version = response.version;
+				const totalMods = response.rawResponse.forgeData['mods'].length;
+				const onlineCount = response.onlinePlayers;
+				const maxPlayers = response.maxPlayers;
+				const players = response.samplePlayers;
+				const srvDesc = response.description['descriptionText'];
+
+				const embed = new MessageEmbed()
+					.setColor(settings.guildcolor)
+					.setTitle(`Creation 2.5 Server - Is Online!`)
+					.setDescription(`**Connection Info›** ${host}:${port}\n**Player Count›** ${onlineCount}/${maxPlayers}`)
+					.addField('Connected Players›', players.map((p) => p.name).join('\n'))
+					.setFooter(`MC Ver - ${version} | Mod Count - ${totalMods}`)
+					.setTimestamp(new Date());
+
+				message.channel.send({ embeds: [embed] }).then((s) => {
+					if (settings.audit) setTimeout(() => s.delete(), 60 * 1000);
+				});
+			})
+			.catch((err) => {
+				const embed = new MessageEmbed().setColor(settings.guildcolor).setTitle(`Creation 2.5 Server - Is Offline!`).setTimestamp(new Date());
+				message.channel.send({ embeds: [embed] }).then((s) => {
+					if (settings.audit) setTimeout(() => s.delete(), 60 * 1000);
+				});
+			});
+	},
+};
--- Storage/Functions/utilFunctions.js
+++ Storage/Functions/utilFunctions.js
@@ -1,17 +1,20 @@
 const moment = require('moment');
 
-module.exports = bot => {
-
+module.exports = (bot) => {
 	// Timestamp
-	bot.Timestamp = date => {
+	bot.Timestamp = (date) => {
 		return moment(date).format('MMMM Do YYYY, h:mm A');
 	};
 
+	bot.relativeTimestamp = (date) => {
+		return `<t:${date / 1000}:R>`;
+	};
+
 	// Is Hex
-	bot.isHex = h => {
-		if(h.startsWith('#')) h = h.slice(1);
+	bot.isHex = (h) => {
+		if (h.startsWith('#')) h = h.slice(1);
 		const a = parseInt(h, 16);
-		return (a.toString(16) === h.toLowerCase());
+		return a.toString(16) === h.toLowerCase();
 	};
 
 	// Date Difference
@@ -21,8 +24,7 @@ module.exports = bot => {
 	};
 
 	// To Thousands
-	bot.toThousands = x => {
+	bot.toThousands = (x) => {
 		return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 	};
-
 };
