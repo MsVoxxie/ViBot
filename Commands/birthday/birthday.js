3e73908d897b4af4dea31e0f9343cf62ea908520
MsVoxxie <odysseylove222@gmail.com>
2021-11-09 16:58:11 -0500

    Checks, Checks, Checks.

--- Commands/birthday/birthday.js
+++ Commands/birthday/birthday.js
@@ -14,13 +14,16 @@ module.exports = {
 	userPerms: [],
 	botPerms: [],
 	async execute(bot, message, args, settings, Vimotes) {
+		//Check if arguments are provided.
+		if (args.length) return message.channel.send('Please do not provide arguments!').then((m) => setTimeout(() => m.delete(), 15 * 1000));
+
 		//Check if birthday already exists
 		var date_regex = /^(0?[1-9]|1[0-2]){1}\/(0?[1-9]|1[0-9]|2[0-9]|3[0-1]){1}$/;
 		const Bdays = await bot.getBirthdays(message.guild);
 		const Users = Bdays.birthdays;
 
 		const BdayCheck = await Users.find((ID) => ID.userid === message.author.id);
-		if (BdayCheck) return message.channel.send(`You already have a birthday set!`).then((m) => setTimeout(() => m.delete(), 5 * 1000));
+		if (BdayCheck) return message.channel.send(`You already have a birthday set!`).then((m) => setTimeout(() => m.delete(), 15 * 1000));
 
 		//Define Variables
 		let EmbedID = undefined;
@@ -35,12 +38,12 @@ module.exports = {
 		await message.channel.awaitMessages({ filter, max: 1, time: 360 * 1000, error: ['time'] }).then(async (collected) => {
 			if (!date_regex.test(collected.first().cleanContent)) {
 				await message.reply('Invalid Date, Cancelling!').then((m) => {
-					setTimeout(() => m.delete(), 5 * 1000);
+					setTimeout(() => m.delete(), 15 * 1000);
 					dateValid = false;
 				});
 			}
 			BirthDate = await Date.parse(collected.first().cleanContent).toLocaleString('en', { dateStyle: 'short' }).replace(/,/g, '');
-			setTimeout(() => collected.first().delete(), 5 * 1000);
+			setTimeout(() => collected.first().delete(), 15 * 1000);
 		});
 
 		if (!dateValid) return;
@@ -50,7 +53,7 @@ module.exports = {
 		EmbedID = await GenerateEmbed(settings.guildcolor, message, EmbedID, 'Awesome!', `Added your birthday! 🎂`, true);
 		await message.channel.awaitMessages({ filter, max: 1, time: 360 * 1000, error: ['time'] }).then(async (collected) => {
 			BirthDate = await Date.parse(collected.first().cleanContent);
-			setTimeout(() => collected.first().delete(), 5 * 1000);
+			setTimeout(() => collected.first().delete(), 15 * 1000);
 		});
 	},
 };
