e55a70300a3504c89f7763861a60351ef0cad376
MsVoxxie <odysseylove222@gmail.com>
2021-12-11 20:39:41 -0500

    FFXIV Start

---
+++ Commands/ffxiv/itemsearch.js
@@ -0,0 +1,63 @@
+const fetch = require('node-fetch');
+const { MessageEmbed } = require('discord.js');
+const XIVAPI = require('@xivapi/js');
+const { XIVAPIKEY, XIVCOL } = require('../../Storage/Config/Config.json');
+const xiv = new XIVAPI({
+	private_key: XIVAPIKEY,
+	language: 'en',
+	snake_case: true,
+});
+const wiki_url = 'https://ffxiv.gamerescape.com/wiki/';
+const api_url = 'https://XIVAPI.com';
+
+module.exports = {
+	name: 'itemsearch',
+	aliases: ['is'],
+	description: 'Search for an item in FFXIV',
+	example: 'is bread',
+	category: 'ffxiv',
+	args: true,
+	cooldown: 2,
+	hidden: false,
+	ownerOnly: false,
+	userPerms: [],
+	botPerms: [],
+	async execute(bot, message, args, settings, Vimotes) {
+		try {
+			//Get Item Search
+			let response = await xiv.search(args.join(' '), { indexes: ['Item'] });
+			response = response.results[0];
+
+			//Get Item Name and Escape spaces
+			const item_name = response.name.replace(/ /g, '_');
+
+			//Get item data from api
+			const fetched_data = await fetch(`${api_url}${response.url}`)
+				.then((res) => res.json())
+				.then((data) => {
+					return data;
+				});
+
+			const item_description = `${fetched_data.Description.split('※')[0]
+				.replace(/(\r\n|\n|\r)/gm, '')
+				.replace(/(<([^>]+)>)/gi, '')}`;
+
+			//Logging
+			// console.log(response);
+			// console.log(`${wiki_url}${item_name}`, `${api_url}${response.url}`);
+
+			//Create embeds
+			const embed = new MessageEmbed()
+				.setTitle(response.name)
+				.setURL(`${wiki_url}${item_name}`)
+				.setColor(XIVCOL)
+				.setThumbnail(`${api_url}${response.icon}`)
+				.setDescription(`${fetched_data.Description ? item_description : ''}`)
+				.setFooter(`• ${fetched_data.ItemUICategory['Name']}`);
+
+			message.channel.send({ embeds: [embed] });
+		} catch (error) {
+			console.log(error);
+		}
+	},
+};
