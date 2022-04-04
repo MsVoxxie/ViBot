5a16033af9328c1898536a3b07962b53ce88e508
MsVoxxie <odysseylove222@gmail.com>
2022-04-04 03:14:40 +0000

    Remove depricated functions

--- Storage/Functions/dbFunctions.js
+++ Storage/Functions/dbFunctions.js
@@ -39,49 +39,6 @@ module.exports = (bot) => {
 		}
 	};
 
-	// // Reaction Roles Create
-	// bot.createReactions = async (settings) => {
-	// 	const defaults = Object.assign({ _id: mongoose.Types.ObjectId() }, bot.reactionDefaults.defaultSettings);
-	// 	const merged = Object.assign(defaults, settings);
-	// 	const newReaction = await new Reaction(merged);
-	// 	const check = await Reaction.findOne({ guildid: merged.guildid });
-	// 	if (check) {
-	// 		return;
-	// 	} else {
-	// 		return newReaction.save().then(console.log(`Created new Reaction Model for \`${merged.guildname}\``));
-	// 	}
-	// };
-
-	// // Get guild Reaction Roles
-	// bot.getReactions = async (guild) => {
-	// 	if (!guild) throw new Error('No Guild Provided!');
-	// 	const data = await Reaction.findOne({ guildid: guild.id });
-	// 	if (data) return data;
-	// 	else return bot.reactionDefaults.defaultSettings;
-	// };
-
-	// // Add Reaction to Guild
-	// bot.addReaction = async (guild, settings) => {
-	// 	if (!guild) throw new Error('No Guild Provided!');
-	// 	const data = await Reaction.findOne({ guildid: guild.id });
-	// 	const reactionRoles = await data.reactionRoles;
-	// 	if (typeof settings !== 'object') return console.log('User did not provide an object, Returning.');
-
-	// 	reactionRoles.push(settings);
-	// 	data.save();
-	// };
-
-	// // Remove Reaction to Guild
-	// bot.removeReaction = async (guild, settings) => {
-	// 	if (!guild) throw new Error('No Guild Provided!');
-	// 	const data = await Reaction.findOne({ guildid: guild.id });
-	// 	const reactionRoles = await data.reactionRoles;
-	// 	if (typeof settings !== 'object') return console.log('User did not provide an object, Returning.');
-
-	// 	reactionRoles.pull(settings);
-	// 	data.save();
-	// };
-
 	// Add Module to List
 	bot.disableModule = async (guild, module) => {
 		if (!guild) throw new Error('No Guild Provided!');
