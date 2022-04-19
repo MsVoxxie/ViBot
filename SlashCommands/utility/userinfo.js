9653f9b96ca2aca5fd35871c08c892241aeeb195
MsVoxxie <odysseylove222@gmail.com>
2022-04-19 00:08:46 +0000

    Formatting

--- SlashCommands/utility/userinfo.js
+++ SlashCommands/utility/userinfo.js
@@ -19,7 +19,7 @@ module.exports = {
         const dbMember = await userData.findOne({ guildid: intGuild.id, userid: member.id }).lean();
 
         //Definitions
-        const currentGames = member.presence.activities.filter(a => a.type === 'PLAYING').map(a => { return `${a.name}${a.details ? `\n${a.details}` : ''}${a.state ? `\n${a.state}` : ''}`}).join('\n\n');
+        const currentGames = member.presence?.activities?.filter(a => a.type === 'PLAYING').map(a => { return `+ ${a.name}${a.details ? `\n› ${a.details}` : ''}${a.state ? `\n› ${a.state}` : ''}\n- (${bot.getDuration(a.timestamps.start).join(' ')})`}).join('\n\n');
 
         //Create embed
         const embed = new MessageEmbed()
@@ -27,14 +27,14 @@ module.exports = {
             .setAuthor({ name: `${member.user.tag}${member.user.bot ? ' - Bot' : ''}`})
             .setThumbnail(member.displayAvatarURL({ dynamic: true }))
             .addField('Nickname', member.nickname ? member.nickname : 'None', true)
-            .addField('Online State', bot.titleCase(member.presence.status), true)
+            .addField('Online State', bot.titleCase(member?.presence?.status ? member.presence.status : 'Offline'), true)
             .addField('Birthday', dbMember.birthday ? moment(Number(dbMember.birthday)).format('MMMM Do') : 'Not Set!', true)
             .addField('Commands Used', dbMember?.commandsused ? dbMember?.commandsused.toString() : 'None, Yet!', true)
             .addField('Joined Server', bot.relativeTimestamp(member.joinedAt), true)
             .addField('Account Created', bot.relativeTimestamp(member.user.createdAt), true)
             .addField('Roles', dbMember.userroles.length ? dbMember.userroles.map((r) => { return `<@&${r.id}>`} ).filter(x => x !== undefined).join(' **|** ') : 'None', false)
-            if(currentGames.length > 0) { embed.addField('Currently Playing', currentGames.length > 0 ? `\`\`\`${currentGames}\`\`\`` : '\`\`\`Not playing anything\`\`\`', false) }
-            if(member.presence.activities.find(a => a.type === 'CUSTOM')) { embed.addField('Custom Status', member.presence.activities.find(a => a.type === 'CUSTOM') ? `\`\`\`${member.presence.activities.find(a => a.type === 'CUSTOM').state}\`\`\`` : '\`\`\`No custom status set\`\`\`', false) }
+            if(currentGames?.length > 0) { embed.addField('Currently Playing - ', currentGames.length > 0 ? `\`\`\`diff\n${currentGames}\`\`\`` : '\`\`\`Not playing anything\`\`\`', false) }
+            if(member.presence?.activities.find(a => a.type === 'CUSTOM')) { embed.addField('Custom Status', member.presence.activities.find(a => a.type === 'CUSTOM') ? `\`\`\`fix\n${member.presence.activities.find(a => a.type === 'CUSTOM').state}\`\`\`` : '\`\`\`No custom status set\`\`\`', false) }
             embed.setFooter({ text: `User ID› ${member.id}` })
             await interaction.reply({ embeds: [embed] });
     },
--- Storage/Functions/utilFunctions.js
+++ Storage/Functions/utilFunctions.js
@@ -17,6 +17,39 @@ module.exports = (bot) => {
 		else return false;
 	};
 
+	//Get Duration
+	bot.getDuration = (startDate, endDate) => {
+		let parts = [];
+		const period = moment(endDate).diff(startDate);
+		const duration = moment.duration(period);
+		if (!duration || duration.toISOString() === 'P0D') return;
+		if (duration.years() >= 1) {
+			const years = Math.floor(duration.years());
+			parts.push(years + ' ' + (years > 1 ? 'years' : 'year'));
+		}
+		if (duration.months() >= 1) {
+			const months = Math.floor(duration.months());
+			parts.push(months + ' ' + (months > 1 ? 'months' : 'month'));
+		}
+		if (duration.days() >= 1) {
+			const days = Math.floor(duration.days());
+			parts.push(days + ' ' + (days > 1 ? 'days' : 'day'));
+		}
+		if (duration.hours() >= 1) {
+			const hours = Math.floor(duration.hours());
+			parts.push(hours + ' ' + (hours > 1 ? 'hours' : 'hour'));
+		}
+		if (duration.minutes() >= 1) {
+			const minutes = Math.floor(duration.minutes());
+			parts.push(minutes + ' ' + (minutes > 1 ? 'minutes' : 'minute'));
+		}
+		if (duration.seconds() >= 1) {
+			const seconds = Math.floor(duration.seconds());
+			parts.push(seconds + ' ' + (seconds > 1 ? 'seconds' : 'second'));
+		}
+		return parts;
+	}
+
 	// Timestamp
 	bot.Timestamp = (date) => {
 		return moment(date).format('MMMM Do YYYY, h:mm A');
