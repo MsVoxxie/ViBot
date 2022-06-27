4e8566532316866d2758fc244f0fa9f32790f79d
MsVoxxie <odysseylove222@gmail.com>
2022-06-27 21:54:11 +0000

    Better updates for roles and channels

--- Events/AuditLogging/channelUpdate.js
+++ Events/AuditLogging/channelUpdate.js
@@ -11,7 +11,7 @@ module.exports = {
 
         // Declarations / Checks
 		if (oldChannel === newChannel) return;
-		if (newChannel.name === oldChannel.name) return;
+		if (newChannel.position !== oldChannel.position) return;
 		const settings = await bot.getGuild(guild);
 		if (!settings) return;
 		if (settings.audit === false) return;
@@ -29,17 +29,30 @@ module.exports = {
 		if (!logChannel) return;
 
 		//Check Permission Changes Do This Later
-		// const oldChannelPerms = oldChannel.permissions.toArray();
-		// const newChannelPerms = newChannel.permissions.toArray();
+		// const oldChannelPerms = oldChannel.permissionOverwrites;
+		// const newChannelPerms = newChannel.permissionOverwrites;
 		// const permissionsAdded = newChannelPerms.filter(x => !oldChannelPerms.includes(x));
 		// const permissionsRemoved = oldChannelPerms.filter(x => !newChannelPerms.includes(x));
+
+		//Check for changes
+		const channelChanges = [
+			{changed : oldChannel.name === newChannel.name ? false : true, data: `${oldChannel.name !== newChannel.name ? `\n› Channel Name\n- ${oldChannel.name}\n+ ${newChannel.name}\n` : ''}`},
+			{changed : oldChannel.topic !== newChannel.topic ? true : false, data: `${oldChannel.topic !== newChannel.topic ? `\n› Channel Topic\n- ${oldChannel.topic}\n+ ${newChannel.topic}\n` : ''}`},
+			{changed : oldChannel.nsfw !== newChannel.nsfw ? true : false, data: `${oldChannel.nsfw !== newChannel.nsfw ? `\n› Channel NSFW\n- ${oldChannel.nsfw}\n+ ${newChannel.nsfw}\n` : ''}`},
+			{changed : oldChannel.rateLimitPerUser !== newChannel.rateLimitPerUser ? true : false, data: `${oldChannel.rateLimitPerUser !== newChannel.rateLimitPerUser ? `\n› Channel Rate Limit\n- ${oldChannel.rateLimitPerUser}s\n+ ${newChannel.rateLimitPerUser}s\n` : ''}`},
+			{changed : oldChannel.userLimit !== newChannel.userLimit ? true : false, data: `${oldChannel.userLimit !== newChannel.userLimit ? `\n› Channel User Limit\n- ${oldChannel.userLimit}\n+ ${newChannel.userLimit}\n` : ''}`},
+			{changed : oldChannel.parentID !== newChannel.parentID ? true : false, data: `${oldChannel.parentID !== newChannel.parentID ? `\n› Channel Parent\n- ${oldChannel.parentID}\n+ ${newChannel.parentID}\n` : ''}`},
+			{changed : oldChannel.position !== newChannel.position ? true : false, data: `${oldChannel.position !== newChannel.position ? `\n› Channel Position\n- ${oldChannel.position}\n+ ${newChannel.position}\n` : ''}`},
+		]
+
+		const changedText = channelChanges.filter(x => x.changed === true).map(x => x.data).join('');
+		const allFalse = channelChanges.every(x => x.changed === false)
+
 
 		// Setup Embed
 		const embed = new MessageEmbed()
 			.setTitle('Channel Updated')
-			.setDescription(`**Updated›** <t:${Math.round(Date.now() / 1000)}:R>\n**Update by›** ${ChannelData ? `<@${ChannelData.executor.id}>` : 'Unknown'}`)
-			.addField('Old Channel', `**Old Name›** ${oldChannel.name}\n**Old Parent›** ${oldChannel.parent ? oldChannel.parent.name : 'None'}`)
-			.addField('Updated Channel', `**New Name›** ${newChannel.name}\n**New Parent›** ${newChannel.parent ? newChannel.parent.name : 'None'}`)
+			.setDescription(`**Updated›** <t:${Math.round(Date.now() / 1000)}:R>\n**Update by›** ${ChannelData ? `<@${ChannelData.executor.id}>` : 'Unknown'}${!allFalse ? `\`\`\`diff\n${changedText}\`\`\`` : ''}`)
 			.setFooter({ text: `Channel ID› ${oldChannel.id}` })
 			.setColor(settings.guildcolor)
 			// if(permissionsAdded.length) embed.addField('Permissions Added', `\`\`\`diff\n${permissionsAdded.map(perm => `+ ${permissions[perm]}`).join('\n')}\`\`\``)
--- Events/AuditLogging/roleUpdate.js
+++ Events/AuditLogging/roleUpdate.js
@@ -7,7 +7,9 @@ module.exports = {
 	disabled: false,
 	once: false,
 	async execute(oldRole, newRole, bot, Vimotes) {
-		if (oldRole === newRole && oldRole.position !== newRole.position) return;
+
+		if (oldRole === newRole) return;
+		if (oldRole.position !== newRole.position) return;
 
 		const guild = newRole.guild;
 		// Declarations / Checks
@@ -33,18 +35,28 @@ module.exports = {
 		const permissionsAdded = newRolePerms.filter(x => !oldRolePerms.includes(x));
 		const permissionsRemoved = oldRolePerms.filter(x => !newRolePerms.includes(x));
 
+		//Check for changes
+		const roleChanges = [
+			{changed : oldRole.name === newRole.name ? false : true, data: `${oldRole.name !== newRole.name ? `\n› Role Name\n- ${oldRole.name}\n+ ${newRole.name}\n` : ''}`},
+			{changed : oldRole.color !== newRole.color ? true : false, data: `${oldRole.color !== newRole.color ? `\n› Role Color\n- #${oldRole.color}\n+ #${newRole.color}\n` : ''}`},
+			{changed : oldRole.hoist !== newRole.hoist ? true : false, data: `${oldRole.hoist !== newRole.hoist ? `\n› Role Hoist\n- #${oldRole.hoist}\n+ #${newRole.hoist}\n` : ''}`},
+			{changed : oldRole.mentionable !== newRole.mentionable ? true : false, data: `${oldRole.mentionable !== newRole.mentionable ? `\n› Role Mentionable\n- ${oldRole.mentionable}\n+ ${newRole.mentionable}\n` : ''}`},
+			{changed : oldRole.position !== newRole.position ? true : false, data: `${oldRole.position !== newRole.position ? `\n› Role Position\n- ${oldRole.position}\n+ ${newRole.position}\n` : ''}`},
+		]
+
+		const changedText = roleChanges.filter(x => x.changed === true).map(x => x.data).join('');
+		const allFalse = roleChanges.every(x => x.changed === false)
+
 		//Generate Colors
 		const colorImage = await bot.createMultiColorCircle([oldRole.hexColor, newRole.hexColor], 1024, 45);
 
 		// Setup Embed
 		const embed = new MessageEmbed()
 			.setTitle('Role Updated')
-			.setDescription(`**Updated›** <t:${Math.round(Date.now() / 1000)}:R>\n**Update by›** ${RoleData ? `<@${RoleData.executor.id}>` : 'Unknown'}`)
-			.addField('Old Role', `**Old Name›** \`${oldRole.name}\`\n**Old Color›** \`${oldRole.hexColor !== '#000000' ? oldRole.hexColor : 'Transparent'}\``)
-			.addField('Updated Role›', `**New Name›** \`${newRole.name}\`\n**New Color›** \`${newRole.hexColor !== '#000000' ? newRole.hexColor : 'Transparent'}\``)
+			.setColor(settings.guildcolor)
 			.setThumbnail('attachment://col.png')
 			.setFooter({ text: `Role ID› ${oldRole.id}` })
-			.setColor(settings.guildcolor)
+			.setDescription(`**Updated›** <t:${Math.round(Date.now() / 1000)}:R>\n**Update by** ${RoleData ? `<@${RoleData.executor.id}>` : 'Unknown'}${!allFalse ? `\`\`\`diff\n${changedText}\`\`\`` : ''}`);
 			if(permissionsAdded.length) embed.addField('Permissions Added', `\`\`\`diff\n${permissionsAdded.map(perm => `+ ${permissions[perm]}`).join('\n')}\`\`\``);
 			if(permissionsRemoved.length) embed.addField('Permissions Removed', `\`\`\`diff\n${permissionsRemoved.map(perm => `- ${permissions[perm]}`).join('\n')}\`\`\``)
 
