86ef21aa8334d531f83084712b73ee02d4fd7eff
MsVoxxie <odysseylove222@gmail.com>
2022-06-27 17:44:36 +0000

    Recursion!

--- Events/LinkFix/tweetFix.js
+++ Events/LinkFix/tweetFix.js
@@ -12,13 +12,15 @@ module.exports = {
 
 		//Checks
 		if (message.author.bot) return;
-		if (!message.content.startsWith('https://twitter.com/')) return;
+		// if (!message.content.startsWith('https://twitter.com/')) return;
 
 		//Declarations
+		const RegEx = /((https?):\/\/)?(www.)?twitter\.com(\/@?(\w){1,15})\/status\/[0-9]{19}/gi;
+		const Matches = [...message.content.matchAll(RegEx)];
 		const settings = await bot.getGuild(message.guild);
 
-		//Get Media
-		const tweetData = await bot.getTwitterMedia(message.content);
+		//Check if Message is a Tweet
+		if (!Matches.length) return;
 
 		await message.react('🐦');
 
@@ -26,38 +28,48 @@ module.exports = {
 		const filter = (reaction, user) => reaction.emoji.name === '🐦' && user.id === message.author.id;
 		const collector = message.createReactionCollector({ filter, time: 60 * 1000, error: ['time'] });
 
-		//Make the lines pretty :)
-		const intData = `♥️ [${bot.toThousands(tweetData.tweet.likes)}] 🔃 [${bot.toThousands(tweetData.tweet.retweets)}]`;
-		const wrapLines = '─'.repeat(bot.MinMax(intData.length / 1.5, 1, 18));
-
 		//Collector Collects
 		collector.on('collect', async () => {
-			const embeds = [];
-			if (!tweetData.tweet.video_url) {
-				for await (const photo of tweetData.tweet.media_urls) {
-					// Create embed
+			//Loop through Matches
+			for await (const Match of Matches) {
+				console.log(Match[0]);
+				//Get Media
+				const tweetData = await bot.getTwitterMedia(Match[0]);
+				//Make the lines pretty :)
+				const intData = `♥️ [${bot.toThousands(tweetData.tweet.likes)}] 🔃 [${bot.toThousands(tweetData.tweet.retweets)}]`;
+				const wrapLines = '─'.repeat(bot.MinMax(intData.length / 1.5, 1, 18));
+
+				//Create Embed
+				const embeds = [];
+				if (!tweetData.tweet.video_url) {
+					if (!tweetData.tweet.media_urls) return await message.channel.send({ content: `Original post by ${message.author}\n${Match[0]}` });
+					for await (const photo of tweetData.tweet.media_urls) {
+						// Create embed for images
+						const embed = new MessageEmbed()
+							.setAuthor({ name: `@${tweetData.user.screen_name}` })
+							.setTitle(tweetData.user.name)
+							.setURL(tweetData.tweet.url)
+							.setThumbnail(tweetData.user.profile_image_url)
+							.setImage(tweetData.tweet.video_url ? null : photo)
+							.setDescription(`${tweetData.tweet.description}\n${wrapLines}\n${intData}\n${wrapLines}`)
+							.setFooter({ text: `Original post by ${message.member.displayName}` })
+							.setColor(settings.guildcolor);
+						embeds.push(embed);
+					}
+					await message.channel.send({ embeds: embeds.map((e) => e) });
+				} else {
+					// Create embed for videos
 					const embed = new MessageEmbed()
 						.setAuthor({ name: `@${tweetData.user.screen_name}` })
 						.setTitle(tweetData.user.name)
 						.setURL(tweetData.tweet.url)
 						.setThumbnail(tweetData.user.profile_image_url)
-						.setImage(tweetData.tweet.video_url ? null : photo)
 						.setDescription(`${tweetData.tweet.description}\n${wrapLines}\n${intData}\n${wrapLines}`)
+						.setFooter({ text: `Original post by ${message.member.displayName}` })
 						.setColor(settings.guildcolor);
-					embeds.push(embed);
+					const attachment = new MessageAttachment(tweetData.tweet.video_url, `media.mp4`);
+					await message.channel.send({ embeds: [embed], files: [attachment] });
 				}
-				await message.channel.send({ content: `Original post by ${message.author}`, embeds: embeds.map((e) => e) });
-			} else {
-				// Create embed
-				const embed = new MessageEmbed()
-					.setAuthor({ name: `@${tweetData.user.screen_name}` })
-					.setTitle(tweetData.user.name)
-					.setURL(tweetData.tweet.url)
-					.setThumbnail(tweetData.user.profile_image_url)
-					.setDescription(`${tweetData.tweet.description}\n${wrapLines}\n${intData}\n${wrapLines}`)
-					.setColor(settings.guildcolor);
-				const attachment = new MessageAttachment(tweetData.tweet.video_url, `media.mp4`);
-				await message.channel.send({ content: `Original post by ${message.author}`, embeds: [embed], files: [attachment] });
 			}
 			await message.delete();
 		});
